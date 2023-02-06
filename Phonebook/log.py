976bb4d90b84216d7afd41c1e2cfb7c9417716d0
Alexei <datlife2015@yandex.ru>
2023-02-06 09:24:50 +0300

    Phonebook done

--- Phonebook/log.py
+++ Phonebook/log.py
@@ -1,10 +1,12 @@
 import datetime
 
 def log_imp(sern):
-    with open('log.txt', 'a', encoding='utf-8') as file:
+    path = 'Phonebook\log.txt'
+    with open(path, 'a', encoding='utf-8') as file:
         file.write(f'Запрос по фамилии {sern} осуществлен в {datetime.datetime.now()} \n')
 
 
 def log_exp(result):
-    with open('log.txt', 'a', encoding='utf-8') as file:
+    path = 'Phonebook\log.txt'
+    with open(path, 'a', encoding='utf-8') as file:
         file.write(f'Данные {result} были записаны в {datetime.datetime.now()} \n')
